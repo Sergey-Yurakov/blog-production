3a41ac46313091d4129c298c92393b458e0689e0
Sergey Yurakov <iurakov@greensight.r>
2023-02-23 16:55:33 +0300

    add modal, portal, change header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,3 @@
-import './styles/index.scss';
-
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames as cn } from 'shared/lib/classNames/classNames';
 import { AppRouter } from 'app/providers/router';
---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,75 @@
+import { classNames as cn } from 'shared/lib/classNames/classNames';
+import React, {
+    ReactNode, useCallback, useEffect, useRef, useState,
+} from 'react';
+import { Portal } from 'shared/ui/Portal/Portal';
+import { useTheme } from 'app/providers/ThemeProvider';
+import cl from './Modal.module.scss';
+
+interface ModalProps {
+    className?: string;
+    children?: ReactNode;
+    isOpen?: boolean;
+    onCLose?: () => void;
+}
+
+const ANIMATION_DELAY = 300;
+export const Modal = (props: ModalProps) => {
+    const {
+        className, children, onCLose, isOpen,
+    } = props;
+
+    const [isClosing, setIsClosing] = useState(false);
+    const timerRef = useRef<ReturnType<typeof setTimeout>>();
+    const { theme } = useTheme();
+
+    const closeHandle = useCallback(() => {
+        if (onCLose) {
+            setIsClosing(true);
+            timerRef.current = setTimeout(() => {
+                onCLose();
+                setIsClosing(false);
+            }, ANIMATION_DELAY);
+        }
+    }, [onCLose]);
+
+    const onKeyDown = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            closeHandle();
+        }
+    }, [closeHandle]);
+
+    const onContextClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+    };
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
+        }
+        return () => {
+            clearInterval(timerRef.current);
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [isOpen, onKeyDown]);
+
+    const mods: Record<string, boolean> = {
+        [cl.opened]: isOpen,
+        [cl.isClosing]: isClosing,
+        [cl[theme]]: true,
+    };
+    return (
+        <Portal>
+            <div className={cn(cl.Modal, mods, [className])}>
+                <div className={cl.overlay} onClick={closeHandle}>
+                    <div
+                        className={cl.content}
+                        onClick={onContextClick}
+                    >
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </Portal>
+    );
+};
