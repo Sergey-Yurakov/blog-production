9cec5e9f99264e146b004cdcf1197632a43d5c6d
Sergey Yurakov <iurakov@greensight.r>
2023-02-23 20:16:45 +0300

    add dark theme from modal

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -19,9 +19,9 @@ Light.args = {
     children: 'Lorem ipsum dolor sit amet, consecrate radicalising elite. Eos, provident.',
 };
 
-// export const Dark = Template.bind({});
-// Dark.args = {
-//     isOpen: true,
-//     children: 'Lorem ipsum dolor sit amet, consecrate radicalising elite. Eos, provident.',
-// };
-// Dark.decorators = [ThemeDecorator(Theme.DARK)];
+export const Dark = Template.bind({});
+Dark.args = {
+    isOpen: true,
+    children: 'Lorem ipsum dolor sit amet, consecrate radicalising elite. Eos, provident.',
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
