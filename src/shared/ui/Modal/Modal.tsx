fe40cc20012c642f301897d082b7c6c1709accbb
Sergey Yurakov <iurakov@greensight.r>
2023-02-25 01:53:07 +0300

    fix modal window, add new screens

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -3,7 +3,7 @@ import React, {
     ReactNode, useCallback, useEffect, useRef, useState,
 } from 'react';
 import { Portal } from 'shared/ui/Portal/Portal';
-// import { useTheme } from 'app/providers/ThemeProvider';
+import { useTheme } from 'app/providers/ThemeProvider';
 import cl from './Modal.module.scss';
 
 interface ModalProps {
@@ -21,7 +21,7 @@ export const Modal = (props: ModalProps) => {
 
     const [isClosing, setIsClosing] = useState(false);
     const timerRef = useRef<ReturnType<typeof setTimeout>>();
-    // const { theme } = useTheme();
+    const { theme } = useTheme();
 
     const closeHandle = useCallback(() => {
         if (onCLose) {
