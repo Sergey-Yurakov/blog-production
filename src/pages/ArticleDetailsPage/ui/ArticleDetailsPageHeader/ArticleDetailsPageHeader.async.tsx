cb259694fc5b762d8fa290322fae1358b4a107ba
Sergey Yurakov <iurakov@greensight.r>
2023-04-04 15:48:29 +0300

    production

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPageHeader/ArticleDetailsPageHeader.async.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPageHeader/ArticleDetailsPageHeader.async.tsx
@@ -1,6 +1,3 @@
 import { lazy } from 'react';
 
-export const ArticleDetailsPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    setTimeout(() => resolve(import('./ArticleDetailsPageHeader')), 400);
-}));
+export const ArticleDetailsPageAsync = lazy(() => import('./ArticleDetailsPageHeader'));
